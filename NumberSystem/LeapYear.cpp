2054b1e365b95409c81d02726e58b447ac737e81
briskgaurav <briskgaurav@gmail.com>
2024-10-03 13:44:54 +0530

    Cpp

---
+++ NumberSystem/LeapYear.cpp
@@ -0,0 +1,17 @@
+
+int addDigits(int num)
+{
+
+    while (num > 9)
+    {
+        int ans = 0;
+        while (num != 0)
+        {
+            int rem = num % 10;
+            num = num / 10;
+            ans = ans + rem;
+        }
+        num = ans;
+    }
+    return num;
+} 
