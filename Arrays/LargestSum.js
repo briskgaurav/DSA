b8105e37aefecc8658c13742f87bdb3e2f174ed1
briskgaurav <briskgaurav@gmail.com>
2024-10-03 13:14:50 +0530

    shifted to C++

---
+++ Arrays/LargestSum.js
@@ -0,0 +1,16 @@
+function LargestSum(arr,n){
+    let max=0;
+    for(i=0;i<arr.length-n+1; i++){
+        let temp = 0;
+        for(j=0;j<n; j++){
+            temp=temp+arr[i+j]
+        }
+        if(temp>max){
+            max=temp;
+        }
+    }
+    return max;
+}
+
+const ans = LargestSum([1,2,3,4,5,6,7,8,9],4)
+console.log(ans);
